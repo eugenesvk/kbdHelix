6ab4fd3663487fcfe399a8b236ad55cd51d2b873
eugenesvk <eugenesvk@users.noreply.github.com>
2023-04-18 18:51:26 +0700

    tooltip: allow custom label ids (positions) per mod

--- static/js/kbd-tooltip.js
+++ static/js/kbd-tooltip.js
@@ -8,20 +8,22 @@ const range = (start, stop, step=1) => Array.from(
 
 // const keyCapLblIDs = range(0, 8); // top 9 key labels only
 const keyCapLblIDs = [0,2,4,6,8]; // but we only need corners + center
+const keyCapLblIDs_ins = [0,1,2,4,6,8]; // and sometimes top
 const modifew_modes_pre	= '#keyboard.modifew-';
 const modifew_modes    	= ['m1NOR','m2INS','m3SEL','nGoTo','nMatch','nSpace','nUnimpaired','nView','nWindow','nHelp'];
-const lbl_modi = new Map([[0,'⇧'],[2,'⎇⇧'],[4,'⎈'],[6,''],[8,'⎇']]); // maps key label ID to a modifier it represents
+const lbl_modi         	= new Map([[0,'⇧'],[2,'⎇⇧'],[4,'⎈'],[6,''],[8,'⎇']]); // maps key label ID to a modifier it represents
+const lbl_modi_ins     	= new Map([[0,'⇧'],[1,'⎈⇧'],[2,'⎇⇧'],[4,'⎈'],[6,''],[8,'⎇']]);
 const modifew_mode_sym = {
-  'm1NOR'      	: {'icon':'Ⓝ'  	, 'path':'keys.normal'      	, 'modi':lbl_modi },
-  'm2INS'      	: {'icon':'ⓘ'  	, 'path':'keys.insert'      	, 'modi':lbl_modi },
-  'm3SEL'      	: {'icon':'Ⓢ'  	, 'path':'keys.select'      	, 'modi':lbl_modi },
-  'nGoTo'      	: {'icon':'☰⮊' 	, 'path':'keys.normal.g'    	, 'modi':lbl_modi },
-  'nMatch'     	: {'icon':'☰🧩' 	, 'path':'keys.normal.n'    	, 'modi':lbl_modi },
-  'nSpace'     	: {'icon':'☰␠' 	, 'path':'keys.normal.space'	, 'modi':lbl_modi },
-  'nUnimpaired'	: {'icon':'⧛☰⧚'	, 'path':'keys.normal.['    	, 'modi':lbl_modi },
-  'nView'      	: {'icon':'☰👁' 	, 'path':'keys.normal.p'    	, 'modi':lbl_modi },
-  'nWindow'    	: {'icon':'☰🗔' 	, 'path':'keys.normal.C-w'  	, 'modi':lbl_modi },
-  'nHelp'      	: {'icon':'☰?' 	, 'path':'keys.normal.F1'   	, 'modi':lbl_modi}
+  'm1NOR'      	: {'icon':'Ⓝ'  	, 'path':'keys.normal'      	, 'modi':lbl_modi    	, 'id':keyCapLblIDs},
+  'm2INS'      	: {'icon':'ⓘ'  	, 'path':'keys.insert'      	, 'modi':lbl_modi_ins	, 'id':keyCapLblIDs_ins},
+  'm3SEL'      	: {'icon':'Ⓢ'  	, 'path':'keys.select'      	, 'modi':lbl_modi    	, 'id':keyCapLblIDs},
+  'nGoTo'      	: {'icon':'☰⮊' 	, 'path':'keys.normal.g'    	, 'modi':lbl_modi    	, 'id':keyCapLblIDs},
+  'nMatch'     	: {'icon':'☰🧩' 	, 'path':'keys.normal.n'    	, 'modi':lbl_modi    	, 'id':keyCapLblIDs},
+  'nSpace'     	: {'icon':'☰␠' 	, 'path':'keys.normal.space'	, 'modi':lbl_modi    	, 'id':keyCapLblIDs},
+  'nUnimpaired'	: {'icon':'⧛☰⧚'	, 'path':'keys.normal.['    	, 'modi':lbl_modi    	, 'id':keyCapLblIDs},
+  'nView'      	: {'icon':'☰👁' 	, 'path':'keys.normal.p'    	, 'modi':lbl_modi    	, 'id':keyCapLblIDs},
+  'nWindow'    	: {'icon':'☰🗔' 	, 'path':'keys.normal.C-w'  	, 'modi':lbl_modi    	, 'id':keyCapLblIDs},
+  'nHelp'      	: {'icon':'☰?' 	, 'path':'keys.normal.F1'   	, 'modi':lbl_modi    	, 'id':keyCapLblIDs}
 };
 const keylabel_path	= '#keyboard-bg .key .keycap .keylabels .keylabel';
 const key_lbl_class	= 'keylabel10';
@@ -33,10 +35,14 @@ function getNestedPath(xpth, map=modifew_mode_sym){
 function getModeKeys(mode){
   const path2icon	= mode + '.icon';
   const path2path	= mode + '.path';
+  const path2modi	= mode + '.modi';
+  const path2id  	= mode + '.id';
   const path2keys	= getNestedPath(path2path,modifew_mode_sym);
   const keys     	= getNestedPath(path2keys,modifew);
   const icon     	= getNestedPath(path2icon,modifew_mode_sym);
-  return [keys,icon];
+  const modis    	= getNestedPath(path2modi,modifew_mode_sym);
+  const ids      	= getNestedPath(path2id,modifew_mode_sym);
+  return [keys,icon,modis,ids];
 }
 
 const keySymbLblMod	= new Map([['⎈','C-'], ['⎇','A-'], ['⇧','S-']]);
@@ -83,7 +89,7 @@ let getSiblings = function (e) {
   }
   return siblings;
 };
-function isValidLblPos(el, validPos = keyCapLblIDs) {
+function isValidLblPos(el, validPos) {
   let retVal = false;
   validPos.map(pos_id => {
     const key_lbl_class = `keylabel${pos_id}`;
@@ -91,14 +97,14 @@ function isValidLblPos(el, validPos = keyCapLblIDs) {
       retVal = true ; } });
   return retVal;
 }
-let getSiblingKeyCaps = function (e) { // get only valid sibling keycap elements
+let getSiblingKeyCaps = function (e, ids) { // get only valid sibling keycap elements
   let siblings = [];
   if(!e.parentNode) { return siblings; }
   let sibling  = e.parentNode.firstChild;
   while (sibling) {
     if (sibling.nodeType === 1 && sibling !== e &&
         sibling.innerText && // keycap label exists...
-        isValidLblPos(sibling) ) { // ... and is one of the top 5 (corners + center)
+        isValidLblPos(sibling, ids) ) { // ... and is one of the valid label positions
       siblings.push(sibling); }
     sibling = sibling.nextSibling;
   }
@@ -123,13 +129,13 @@ function pt(...items) { // helper print var's type and var's value
 function reLastLetter(letter) { // get the regex that matches 'b' but not 'tab' for 'b' or 'B'
   return new RegExp('(?<![a-z])'+escRe(letter)+'$', 'i');
 }
-function getKeyCombo(k_in, keys, modi=lbl_modi) { // for 'b' get ⇧=>switch_to_lowercase, ⎈=>switch_to_uppercase ''=>no_op at each label respectively: {'0' => {…}, '4' => {…}, '6' => {…}}
+function getKeyCombo(k_in, keys, lbl_modis=lbl_modi) { // for 'b' get ⇧=>switch_to_lowercase, ⎈=>switch_to_uppercase ''=>no_op at each label respectively: {'0' => {…}, '4' => {…}, '6' => {…}}
   // from {B:..lower, A-tab:move, C-b:...upper, b:no_op}
   const k = keySymbToLbl(k_in).toLowerCase();
   let key_fmt, cmd;
   let keyCombo = new Map();
   function setKeyComboItem(modi,lbl_id) {
-    if (key_fmt === lbl_modi.get(lbl_id)) {
+    if (key_fmt === lbl_modis.get(lbl_id)) {
       keyCombo.set(lbl_id,{'modi':key_fmt, 'cmd':cmd});
     }
   }
@@ -138,7 +144,7 @@ function getKeyCombo(k_in, keys, modi=lbl_modi) { // for 'b' get ⇧=>switch_to_
     if (reLastK.test(key)) {
       key_fmt	= keyLblToSymbMod(key).replace(new RegExp(escRe(k)+'$'),'');
       cmd    	= keys[key];
-      lbl_modi.forEach(setKeyComboItem);
+      lbl_modis.forEach(setKeyComboItem);
     }
   }
   return keyCombo;
@@ -197,8 +203,9 @@ modifew_modes.map(m => {
     const key_cap_el	= [];
 
     if (keyLbl && keyLbl !== 'mods') { // now that we know key label, store all cap symbols for this key
+      const [keys, keyIcon, lbl_modis, cap_ids] = getModeKeys(m);
       const keylbl 	= keyLbl[0].toLowerCase(); // take only the 1st label (number keys have duplicate 1!)
-      const keyCaps	= getSiblingKeyCaps(el); // get all keycaps with valid labels and one of 5 valid positions
+      const keyCaps	= getSiblingKeyCaps(el, cap_ids); // get all keycaps with valid labels and one of 5 valid positions
       // pp({mode},{keyCaps},{keylbl});
 
       if (key_cap_sym.has(keylbl)) { // avoid looping for cap symbols if we already have them
@@ -215,7 +222,6 @@ modifew_modes.map(m => {
         // p('stored keymap for=¦',keylbl,'¦',key_cap_sym.get(keylbl));
       }
       // Generate tooltip table
-      const [keys, keyIcon]	= getModeKeys(m);
       let tt_div           	= document.createElement('div');
       let tt_table         	= document.createElement('table');
       tt_table.classList.add('styled-table');
@@ -225,9 +231,9 @@ modifew_modes.map(m => {
       tt_div.appendChild(tt_table);
       setTableHead(tt_table, table_header);
       const cLytLbl  	= gLyt.lbl;  // reads layout only at page load
-      const key_combo	= getKeyCombo(keylbl, keys); // {0:'⇧'=>'switch_to_lowercase'>..}
       let showTT     	= false; // hide empty tooltips (header without rows)
-      keyCapLblIDs.map(lbl_id => {
+      const key_combo	= getKeyCombo(keylbl, keys, lbl_modis); // {0:'⇧'=>'switch_to_lowercase'>..}
+      cap_ids.map(lbl_id => {
         const lbl_id_s	 = lbl_id.toString();
         if (!key_combo.has(lbl_id)) { return; } // break sequence as no combos for this label
         const key_mod_cmd	 = key_combo.get(lbl_id);
