597c546774ff7ccd822cdc2c26f4d79198f3b132
eugenesvk <eugenesvk@users.noreply.github.com>
2023-04-19 22:20:18 +0700

    update layout converter script data structure
    fix wrong backtick escaping

--- static/js/layout-convert.js
+++ static/js/layout-convert.js
@@ -1,21 +1,33 @@
 export let gLyt = { lbl: "q" };
 const layout_string = {
-  'qwerty'    	: String.raw`\`-=`+`qwertyuiop[]`+`asdfghjkl;'`+`zxcvbnm,./` + `!@#$%^&*()_+`+`QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>?'`,
-  'dvorak'    	: String.raw`\`[]`+`',.pyfgcrl/=`+`aoeuidhtns-`+`;qjkxbmwvz` + `!@#$%^&*(){}`+`"<>PYFGCRL?+AOEUIDHTNS_:QJKXBMWVZ'`,
-  'colemak'   	: String.raw`\`-=`+`qwfpgjluy;[]`+`arstdhneio'`+`zxcvbkm,./` + `!@#$%^&*()_+`+`QWFPGJLUY:{}ARSTDHNEIO"ZXCVBKM<>?'`,
-  'workman'   	: String.raw`\`-=`+`qdrwbjfup;[]`+`ashtgyneoi'`+`zxmcvkl,./` + `!@#$%^&*()_+`+`QDRWBJFUPP{}ASHTGYNEOI"ZXMCVKL<>?'`,
-  'asset'     	: String.raw`\`-=`+`qwjfgypul;[]`+`asetdhnior'`+`zxcvbkm,./` + `!@#$%^&*()_+`+`QWJFGYPUL;{}ASETDHNIO:"ZXCVBKM<>?'`,
-  'colemak_dh'	: String.raw`\`-=`+`qwfpbjluy;[]`+`arstgkneio'`+`zxcdvmh,./` + `!@#$%^&*()_+`+`QWFPBJLUY:{}ARSTGKNEIO"ZXCDVMH<>?'`,
-  'neo2'      	: String.raw`^-\`` +`xvlcwkhgfqß´`+`uiaeosnrtdy`+`üöäpzbm,.j` + `°§ℓ»«$€„“”—¸`+`XVLCWKHGFQẞ˜UIAEOSNRTDYÜÖÄPZBM–•J`,
+  'qwerty.low'    	: String.raw`${"`"}1234567890-=`     +`qwertyuiop[]`+`asdfghjkl;'`+`zxcvbnm,./`,
+  'qwerty.upp'    	: String.raw     `~!@#$%^&*()_+`     +`QWERTYUIOP{}`+`ASDFGHJKL:"`+`ZXCVBNM<>?`,
+  'dvorak.low'    	: String.raw`${"`"}1234567890[]`     +`',.pyfgcrl/=`+`aoeuidhtns-`+`;qjkxbmwvz`,
+  'dvorak.upp'    	: String.raw     `~!@#$%^&*(){}`     +`"<>PYFGCRL?+`+`AOEUIDHTNS_`+`:QJKXBMWVZ`,
+  'colemak.low'   	: String.raw`${"`"}1234567890-=`     +`qwfpgjluy;[]`+`arstdhneio'`+`zxcvbkm,./`,
+  'colemak.upp'   	: String.raw     `~!@#$%^&*()_+`     +`QWFPGJLUY:{}`+`ARSTDHNEIO"`+`ZXCVBKM<>?`,
+  'workman.low'   	: String.raw`${"`"}1234567890-=`     +`qdrwbjfup;[]`+`ashtgyneoi'`+`zxmcvkl,./`,
+  'workman.upp'   	: String.raw     `~!@#$%^&*()_+`     +`QDRWBJFUPP{}`+`ASHTGYNEOI"`+`ZXMCVKL<>?`,
+  'asset.low'     	: String.raw`${"`"}1234567890-=`     +`qwjfgypul;[]`+`asetdhnior'`+`zxcvbkm,./`,
+  'asset.upp'     	: String.raw     `~!@#$%^&*()_+`     +`QWJFGYPUL;{}`+`ASETDHNIO:"`+`ZXCVBKM<>?`,
+  'colemak_dh.low'	: String.raw`${"`"}1234567890-=`     +`qwfpbjluy;[]`+`arstgkneio'`+`zxcdvmh,./`,
+  'colemak_dh.upp'	: String.raw     `~!@#$%^&*()_+`     +`QWFPBJLUY:{}`+`ARSTGKNEIO"`+`ZXCDVMH<>?`,
+  'neo2.low'      	: String.raw     `^1234567890-${"`"}`+`xvlcwkhgfqß´`+`uiaeosnrtdy`+`üöäpzbm,.j`,
+  'neo2.upp'      	: String.raw     `ˇ°§ℓ»«$€„“”—¸`     +`XVLCWKHGFQẞ˜`+`UIAEOSNRTDY`+`ÜÖÄPZBM–•J`,
 };
-export const lyt = {
-  'q':'qwerty','w':'workman','d':'dvorak','c':'colemak','a':'asset','cdh':'colemak_dh','n':'neo2',
-  'qwerty':'q','workman':'w','dvorak':'d','colemak':'c','asset':'a','colemak_dh':'cdh','neo2':'n'};
-const button_name   	= ["btn_qwerty","btn_dvorak","btn_colemak","btn_colemak_dh","btn_workman","btn_neo2",];
-const btn_prefix_len	= "btn_".length
-const buttons       	= document.getElementsByClassName("btn"); // get a list of buttons to style later
-const class_clicked 	= 'layout_button_clicked'; // class that applies a colored border
-const btn_default   	= 'btn_qwerty'; // default layout before any button is clicked
+export const layouts = {'qwerty':'q','dvorak':'d','colemak':'c','colemak_dh':'cdh','workman':'w','neo2':'n','asset':'a'};
+const button_name	= [];
+export const lyt 	= {}; // q:qwerty, qwerty:q
+for (const [lytName, lytAbbrv] of Object.entries(layouts)) {
+  layout_string[lytName] = layout_string[lytName+'.low'] + layout_string[lytName+'.upp'];
+  button_name.push('btn_'+lytName);
+  lyt[lytName] 	= lytAbbrv;
+  lyt[lytAbbrv]	= lytName;
+}
+const btn_prefix_len	= "btn_".length;
+const buttons       	= document.getElementsByClassName("btn")	; // get a list of buttons to style later
+const class_clicked 	= 'layout_button_clicked'               	; // class that applies a colored border
+const btn_default   	= 'btn_qwerty'                          	; // default layout before any button is clicked
 
 export function convert(src, from, to) {
   let ret         	= '';
@@ -43,9 +55,9 @@ const changeKLELayoutTo = ((layoutFrom,layoutTo) => { // change labels of the KL
     if (layoutFrom === layoutTo) {
     } else {
       const thisLbl	= el.textContent;
-      let conv_l = (thisLbl === 'ẞ' ? 'ß' : thisLbl.toLowerCase())
-      let conv   = convert(conv_l, lyt[layoutFrom], lyt[layoutTo])
-      let conv_u = (conv === 'ß' ? 'ẞ' : conv.toUpperCase())
+      let conv_l = (thisLbl === 'ẞ' ? 'ß' : thisLbl.toLowerCase());
+      let conv   = convert(conv_l, lyt[layoutFrom], lyt[layoutTo]);
+      let conv_u = (conv === 'ß' ? 'ẞ' : conv.toUpperCase());
       el.textContent	= conv_u || thisLbl;
     }
   }
