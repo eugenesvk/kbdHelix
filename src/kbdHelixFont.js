a25b6e9f826d5f4fcaad9df8a9e15761d06593f9
eugenesvk <eugenesvk@users.noreply.github.com>
2023-04-28 12:24:43 +0700

    add camera

--- src/kbdHelixFont.js
+++ src/kbdHelixFont.js
@@ -11,10 +11,10 @@ console.log("@kbdHelixFont.js: empty message to suppress emtpy chunk warning");
 // var camera2 = window.FontAwesome.icon({ prefix: 'fas', iconName: 'camera' })
 
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
-import { faUserAstronaut } from '@fortawesome/free-solid-svg-icons'
+import { faUserAstronaut, faCamera } from '@fortawesome/free-solid-svg-icons'
 
 // We are only using the user-astronaut icon
-library.add(faUserAstronaut)
+library.add(faUserAstronaut, faCamera)
 
 // Replace any existing <i> tags with <svg> and set up a MutationObserver to
 // continue doing this as the DOM changes.
