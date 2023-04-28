e7876bedf778b311d2e39fa03b3c0404b33b2067
eugenesvk <eugenesvk@users.noreply.github.com>
2023-04-28 12:15:08 +0700

    _mutatin observer works!!

--- src/kbdHelixFont.js
+++ src/kbdHelixFont.js
@@ -2,9 +2,20 @@ import './css/kbdHelixFont.sass';
 
 console.log("@kbdHelixFont.js: empty message to suppress emtpy chunk warning");
 
-import { library, icon } from '@fortawesome/fontawesome-svg-core'
-import { faCamera } from '@fortawesome/free-solid-svg-icons'
+// import { library, icon } from '@fortawesome/fontawesome-svg-core'
+// import { faCamera } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faCamera)
+// library.add(faCamera)
 
-const camera = icon({ prefix: 'fas', iconName: 'camera' })
+// const camera = icon({ prefix: 'fas', iconName: 'camera' })
+// var camera2 = window.FontAwesome.icon({ prefix: 'fas', iconName: 'camera' })
+
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { faUserAstronaut } from '@fortawesome/free-solid-svg-icons'
+
+// We are only using the user-astronaut icon
+library.add(faUserAstronaut)
+
+// Replace any existing <i> tags with <svg> and set up a MutationObserver to
+// continue doing this as the DOM changes.
+dom.watch()
