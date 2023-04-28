08804e30a5bed1c647e6466d7e18c39c3be128f5
eugenesvk <eugenesvk@users.noreply.github.com>
2023-04-28 11:46:19 +0700

    add example facamera

--- src/kbdHelixFont.js
+++ src/kbdHelixFont.js
@@ -1,3 +1,10 @@
 import './css/kbdHelixFont.sass';
 
 console.log("@kbdHelixFont.js: empty message to suppress emtpy chunk warning");
+
+import { library, icon } from '@fortawesome/fontawesome-svg-core'
+import { faCamera } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faCamera)
+
+const camera = icon({ prefix: 'fas', iconName: 'camera' })
