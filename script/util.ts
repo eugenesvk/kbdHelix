5ddd1ddd3c0e8e63d9f5019ea272556310a9256b
eugenesvk <eugenesvk@users.noreply.github.com>
2023-04-30 14:38:23 +0700

    util: add require resolve util matching non-ESM behavior

--- script/util.ts
+++ script/util.ts
@@ -1,3 +1,4 @@
+import {createRequire}	from 'node:module';
 import {resolve}      	from 'node:path'
 import {bgCyan, black}	from 'kolorist'
 import {fileURLToPath}	from 'url';
@@ -7,6 +8,7 @@ const __dirname       	= dirname(__filename);
 
 export const port  	= parseInt(process.env.PORT || '') || 1111
 export const r     	= (...args: string[]) => resolve(__dirname, '..', ...args)
+export const rr    	= (request, ...opts) => createRequire(import.meta.url).resolve(request, ...opts)
 export const isProd	= process.env.NODE_ENV === 'production'
 export const isDev 	= !isProd
 export const isWin 	= process.platform === "win32";
